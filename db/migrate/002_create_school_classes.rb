60d520bed9be0905a52ba892595a52fe49016c18
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:39:10 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,4 +1,4 @@
-class CreateSchoolClass < ActiveRecord::Migration
+class CreateSchoolClasses < ActiveRecord::Migration
   create_table :school_classes do |t|
     t.string :title
     t.integer :room_number 
