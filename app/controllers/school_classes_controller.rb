bbdc58e5dc7c01eb4f82a2a583cb72508e3d9d4d
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:32:12 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,21 +1,28 @@
 class SchoolClassesController < ApplicationController
- def new 
-    @school_class = SchoolClass.new 
+  def index 
+    @school_class = SchoolClasses.all
+  
+  def new 
+    @school_class = SchoolClasses.new 
   end
   
   def show 
-    @school_classes = SchoolClass.all 
+    @school_classes = SchoolClasses.find(params[:id])
   end 
   
   def create 
-    @school_class = SchoolClass.create(params)
+    @school_class = SchoolClasses.new
+    @school_class.title = params[:title]
+    @school_class.description = params[:description]
     @school_class.save 
     redirect_to school_class_path(@school_class)
   end
   
   def edit 
+    @school_classes = SchoolClasses.find(params[:id])
   end
   
   def update 
+    
   end 
 end
