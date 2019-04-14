6ec5fa3c582bd0ee4519278b34c3f0ac16d84fbd
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:28:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,5 +24,8 @@ class StudentsController < ApplicationController
   end
   
   def update 
+    @student = Student.find(params[:id])
+	  @student.update(params.require(:student).permit(:first_name, :last_name))
+	  redirect_to post_path(@student)
   end 
 end
