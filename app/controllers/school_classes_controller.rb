278839c8a8a0f2231c254765a617d146fb3df2cd
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:19:18 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,21 @@
 class SchoolClassesController < ApplicationController
- 
+ def new 
+    @school_class = SchoolClass.new 
+  end
+  
+  def show 
+    @school_classes = SchoolClass.all 
+  end 
+  
+  def create 
+    @school_class = SchoolClass.create(params)
+    @school_class.save 
+    redirect_to school_class_path(@school_class)
+  end
+  
+  def edit 
+  end
+  
+  def update 
+  end 
+end
