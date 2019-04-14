8344b3b3041dfad66105524e27822228847983f1
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:29:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :school_class, only: [:index, :create, :new, :show, :edit, :update]
   resources :students, only: [:index, :create, :new, :show, :edit, :update]
+  patch 'students/:id', to: 'students#update'
 end
