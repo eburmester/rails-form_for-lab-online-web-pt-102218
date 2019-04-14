da498d24e4a32c7d41b9fa9e8f352b20bbd7e0ad
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 19:13:24 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -25,7 +25,7 @@ class SchoolClassesController < ApplicationController
   
   def update 
     @school_class = SchoolClass.find(params[:id])
-	  @school_class.update(params.require(:school_class).permit(:title, :room_number))
+	  @school_class.update(params.require(:school_classes).permit(:title, :room_number))
 	  redirect_to school_class_path(@school_class)
   end 
 end
