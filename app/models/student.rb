bee580ec7f54bc05c63323c3949aec0e6d2f2797
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:18:38 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,25 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+ 
+  def new 
+    @student = Students.new
+  end
+  
+  def show 
+    @students = Student.all 
+  end 
+  
+  def create 
+    @student = Students.create(params)
+    @student.save 
+    redirect_to students_path(@student)
+  end
+  
+  def edit 
+  end
+  
+  def update 
+  end 
+  
 end
