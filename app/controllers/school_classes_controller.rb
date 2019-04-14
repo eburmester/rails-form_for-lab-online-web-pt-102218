e41cf150cc1bc4c23aaae8ce403526752c6bfe4a
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:55:56 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -14,7 +14,7 @@ class SchoolClassesController < ApplicationController
   def create 
     @school_class = SchoolClass.new
     @school_class.title = params[:title]
-    @school_class.description = params[:description]
+    @school_class.room_number = params[:room_number]
     @school_class.save 
     redirect_to school_class_path(@school_class)
   end
@@ -25,7 +25,7 @@ class SchoolClassesController < ApplicationController
   
   def update 
     @school_class = SchoolClass.find(params[:id])
-	  @school_class.update(params.require(:school_class).permit(:title, :description))
+	  @school_class.update(params.require(:school_class).permit(:title, :room_number))
 	  redirect_to post_path(@school_class)
   end 
 end
