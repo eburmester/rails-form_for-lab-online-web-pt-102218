c2e27b276dd6f340c325791c9b2f0f5b6ed0c4ea
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:37:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,11 @@ ActiveRecord::Schema.define(version: 2) do
     t.integer "room_number"
   end
 
+  create_table "school_class", force: :cascade do |t|
+    t.string  "title"
+    t.integer "room_number"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
