ff2f91601f0abde29ff23bdc9adca7ee717728db
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:18:17 +0000

    Automatically backed up by Learn

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,3 +1,22 @@
 class SchoolClass < ActiveRecord::Base 
+   def new 
+    @school_class = SchoolClass.new 
+  end
+  
+  def show 
+    @school_classes = SchoolClass.all 
+  end 
+  
+  def create 
+    @school_class = SchoolClass.create(params)
+    @school_class.save 
+    redirect_to school_class_path(@school_class)
+  end
+  
+  def edit 
+  end
+  
+  def update 
+  end 
   
 end
