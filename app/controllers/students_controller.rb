26c088ef82f100cb8563a91d4740bd911cd4815f
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-14 18:22:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
   
   def create 
     @student = Students.create(params)
+    @student.
     @student.save 
     redirect_to students_path(@student)
   end
